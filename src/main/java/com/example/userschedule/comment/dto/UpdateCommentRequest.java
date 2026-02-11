4bc22d732072c8d3a94aa6abd42afffdf40abdec
Ho-jin98 <hohoj98@naver.com>
2026-02-11 23:22:23 +0900

    Feat: Comment dto develop

--- src/main/java/com/example/userschedule/comment/dto/UpdateCommentRequest.java
+++ src/main/java/com/example/userschedule/comment/dto/UpdateCommentRequest.java
@@ -1,8 +1,10 @@
 package com.example.userschedule.comment.dto;
 
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UpdateCommentRequest {
+    @Size(min = 5, max = 50, message = "5~50글자를 입력해주세요.")
     private String reply;
 }
