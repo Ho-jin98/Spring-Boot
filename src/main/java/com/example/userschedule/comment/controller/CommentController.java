537c35bcbe6b46e72634eb5697e6af38e990f0cb
Ho-jin98 <hohoj98@naver.com>
2026-02-11 23:14:55 +0900

    Feat: CommentController develop

--- src/main/java/com/example/userschedule/comment/controller/CommentController.java
+++ src/main/java/com/example/userschedule/comment/controller/CommentController.java
@@ -2,6 +2,7 @@ package com.example.userschedule.comment.controller;
 
 import com.example.userschedule.comment.dto.*;
 import com.example.userschedule.comment.service.CommentService;
+import com.example.userschedule.common.CommonResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,31 +17,35 @@ public class CommentController {
     private CommentService commentService;
 
     @PostMapping("/users/{userId}/schedules/{scheduleId}/comments")
-    public ResponseEntity<CreateCommentResponse> createComment(
+    public ResponseEntity<CommonResponse<CreateCommentResponse>> createComment(
             @PathVariable Long userId,
             @PathVariable Long scheduleId,
             @Valid @RequestBody CreateCommentRequest request){
-        return ResponseEntity.status(HttpStatus.CREATED).body(commentService.save(userId,scheduleId,request));
+        return ResponseEntity.status(HttpStatus.CREATED).body(CommonResponse.success(HttpStatus.CREATED, "댓글 생성 성공",
+                commentService.save(userId,scheduleId,request)));
     }
 
     @GetMapping("/users/{userId}/schedules/{scheduleId}/comments/{commentId}")
-    public ResponseEntity<GetCommentResponse> getComment(@PathVariable Long scheduleId, @PathVariable Long commentId){
-        return ResponseEntity.status(HttpStatus.OK).body(commentService.getComment(scheduleId, commentId));
+    public ResponseEntity<CommonResponse<GetCommentResponse>> getComment(@PathVariable Long scheduleId, @PathVariable Long commentId){
+        return ResponseEntity.status(HttpStatus.OK).body(CommonResponse.success(HttpStatus.OK, "조회 성공",
+                commentService.getComment(scheduleId, commentId)));
     }
 
     @GetMapping("/users/{userId}/schedules/{scheduleId}/comments")
-    public ResponseEntity<List<GetCommentResponse>> getComments(
+    public ResponseEntity<CommonResponse<List<GetCommentResponse>>> getComments(
             @PathVariable Long scheduleId){
-        return ResponseEntity.status(HttpStatus.OK).body(commentService.getComments(scheduleId));
+        return ResponseEntity.status(HttpStatus.OK).body(CommonResponse.success(HttpStatus.OK, "조회 성공",
+                commentService.getComments(scheduleId)));
     }
 
     @PutMapping("/users/{userId}/schedules/{scheduleId}/comments/{commentId}")
-    public ResponseEntity<UpdateCommentResponse> updateComment(
+    public ResponseEntity<CommonResponse<UpdateCommentResponse>> updateComment(
             @PathVariable Long commentId,
             @PathVariable Long userId,
             @PathVariable Long scheduleId,
             @Valid @RequestBody UpdateCommentRequest request) {
-        return ResponseEntity.status(HttpStatus.OK).body(commentService.update(commentId, userId, scheduleId, request));
+        return ResponseEntity.status(HttpStatus.OK).body(CommonResponse.success(HttpStatus.OK, "변경 완료",
+                commentService.update(commentId, userId, scheduleId, request)));
     }
 
     @DeleteMapping("/users/{userId}/schedules/{scheduleId}/comments/{commentId}")
